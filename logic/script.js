239440ba995332fa1f84c659ff750e24d1c1264e
seblessa <sebastiaolessa2002@outlook.com>
2023-10-09 19:04:37 +0100

    Start

---
+++ logic/script.js
@@ -0,0 +1,5 @@
+function login(){
+    console.log("\nLogin attempted:")
+    console.log("Username: " + document.getElementById("username").value)
+    console.log("Password: " + document.getElementById("password").value)
+}
