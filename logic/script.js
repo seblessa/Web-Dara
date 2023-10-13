5e914ba78b05aebb118f9b5e210596dbc3d49f26
seblessa <sebastiaolessa2002@outlook.com>
2023-10-13 16:57:13 +0100

    Working

--- logic/script.js
+++ logic/script.js
@@ -1,3 +1,57 @@
+const pieces = document.querySelectorAll('.piece1, .piece2');
+const cells = document.querySelectorAll('.cell');
+
+let draggedPiece = null;
+
+// Event listener for when a piece starts to be dragged
+pieces.forEach(piece => {
+    piece.addEventListener('dragstart', dragStart);
+    piece.setAttribute('draggable', true);
+});
+
+// Event listener for when a piece is being dragged over a cell
+cells.forEach(cell => {
+    cell.addEventListener('dragover', dragOver);
+    cell.addEventListener('dragenter', dragEnter);
+    cell.addEventListener('dragleave', dragLeave);
+    cell.addEventListener('drop', dragDrop);
+});
+
+// Function to handle the drag start
+function dragStart(event) {
+    draggedPiece = event.target;
+    setTimeout(() => (event.target.style.display = 'none'), 0);
+}
+
+// Functions to handle the drag over, enter, leave, and drop events
+function dragOver(event) {
+    event.preventDefault();
+}
+
+function dragEnter(event) {
+    event.preventDefault();
+    this.classList.add('hovered');
+}
+
+function dragLeave() {
+    this.classList.remove('hovered');
+}
+
+function dragDrop() {
+    this.classList.remove('hovered');
+    this.appendChild(draggedPiece);
+    draggedPiece.style.display = 'block';
+}
+
+
+
+
+
+
+
+
+
+
 function login(){
     console.log("\nLogin attempted:")
     console.log("Username: " + document.getElementById("username").value)
