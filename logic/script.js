b79cb3be727a6901c61e12bf21557bb5f0f9b61b
Diogo Alves <diogomoura2014@gmail.com>
2023-11-06 04:20:37 +0000

    fixed

--- logic/script.js
+++ logic/script.js
@@ -13,7 +13,7 @@ function login() {
   const usernameInput = document.getElementById("username").value;
   const passwordInput = document.getElementById("password").value;
 
-  if (usernameInput === user.username && passwordInput === user.password) {
+  if (usernameInput == user.username && passwordInput == user.password) {
     isLoggedIn = true;
     hideLoginDiv();
     showWelcomeMessage(usernameInput);
@@ -100,22 +100,22 @@ function generatePossibleMoves(x, y) {
     }
   }
   if (x - 1 >= 0) {
-    if (board[x - 1][y] === 0) {
+    if (board[x - 1][y] == 0) {
       possibleMoves[x - 1][y] = 1;
     }
   }
   if (x + 1 < rows) {
-    if (board[x + 1][y] === 0) {
+    if (board[x + 1][y] == 0) {
       possibleMoves[x + 1][y] = 1;
     }
   }
   if (y - 1 >= 0) {
-    if (board[x][y - 1] === 0) {
+    if (board[x][y - 1] == 0) {
       possibleMoves[x][y - 1] = 1;
     }
   }
   if (y + 1 < columns) {
-    if (board[x][y + 1] === 0) {
+    if (board[x][y + 1] == 0) {
       possibleMoves[x][y + 1] = 1;
     }
   }
@@ -138,6 +138,7 @@ function MoreThanThreeRow(x, y) {
   let res = false;
   for (let i = x - 3; i < 7; i++) {
     if (i < 0) continue;
+    if (i > columns) break;
     if (board[i][y] == board[i + 1][y] && board[i + 1][y] == board[i + 2][y]) {
       count++;
     }
@@ -147,6 +148,7 @@ function MoreThanThreeRow(x, y) {
   count = 0;
   for (let i = y - 3; i < 7; i++) {
     if (i < 0) continue;
+    if (i > rows) break;
     if (board[x][i] == board[x][i + 1] && board[x][i + 1] == board[x][i + 2]) {
       count++;
     }
@@ -180,7 +182,7 @@ function renderPossibleMoves(x, y) {
   const cells = document.querySelectorAll(".cell");
   cells.forEach(function (cell) {
     let [x, y] = cell.id.split("-");
-    if (possibleMoves[parseInt(x)][parseInt(y)] === 1) {
+    if (possibleMoves[parseInt(x)][parseInt(y)] == 1) {
       const possibleMove = document.createElement("div");
       possibleMove.classList.add("possible-move");
       cell.appendChild(possibleMove);
@@ -191,15 +193,15 @@ function renderPossibleMoves(x, y) {
     possibleMove.addEventListener("click", function () {
       let [startX, startY] = selected_piece.parentNode.id.split("-");
       let [x, y] = possibleMove.parentNode.id.split("-");
-      if (phase === "Move") {
-        if (playerTurn === "player1") {
+      if (phase == "Move") {
+        if (playerTurn == "player1") {
           makeMove(x, y, startX, startY, possibleMove);
           console.log("player2", ThreeRow(parseInt(x), parseInt(y)));
           playerTurn = "player2";
-        } else if (playerTurn === "player2") {
+        } else if (playerTurn == "player2") {
           makeMove(x, y, startX, startY, possibleMove);
           console.log("player2", ThreeRow(parseInt(x), parseInt(y)));
-          console.log("ola", MoreThanThreeRow(parseInt(x), parseInt(y)));
+          //console.log("ola", MoreThanThreeRow(parseInt(x), parseInt(y)));
           playerTurn = "player1";
         }
       }
@@ -238,14 +240,14 @@ function startGame(difficulty) {
 
   // Define a function to handle the AI's move based on the chosen difficulty
   function handleAIMove() {
-    if (difficulty === "Easy") {
+    if (difficulty == "Easy") {
       // Implement a random player AI logic here
 
       // Generate a random available move
       const availableMoves = [];
       for (let i = 0; i < rows; i++) {
         for (let j = 0; j < columns; j++) {
-          if (board[i][j] === 0) {
+          if (board[i][j] == 0) {
             availableMoves.push({ x: i, y: j });
           }
         }
@@ -264,10 +266,10 @@ function startGame(difficulty) {
         // Update the remaining pieces count for the AI
         DropabbleOpponentPieces--;
       }
-    } else if (difficulty === "Medium") {
+    } else if (difficulty == "Medium") {
       // Implement a low-depth miniMax AI logic here
       // Example: makeMiniMaxMoveLowDepth();
-    } else if (difficulty === "Hard") {
+    } else if (difficulty == "Hard") {
       // Implement a deep-depth miniMax AI logic here
       // Example: makeMiniMaxMoveDeepDepth();
     }
@@ -276,26 +278,26 @@ function startGame(difficulty) {
   cells.forEach(function (cell) {
     cell.addEventListener("click", function () {
       let [x, y] = cell.id.split("-");
-      if (phase === "Drop") {
-        if (playerTurn === "player1" && DropabblePlayerPieces > 0) {
-          if (board[parseInt(x)][parseInt(y)] === 0) {
+      if (phase == "Drop") {
+        if (playerTurn == "player1" && DropabblePlayerPieces > 0) {
+          if (board[parseInt(x)][parseInt(y)] == 0) {
             board[parseInt(x)][parseInt(y)] = 1;
             cell.appendChild(playerPieces.lastChild);
             playerTurn = "player2";
             DropabblePlayerPieces--;
           }
-        } else if (playerTurn === "player2" && DropabbleOpponentPieces > 0) {
-          if (board[parseInt(x)][parseInt(y)] === 0) {
+        } else if (playerTurn == "player2" && DropabbleOpponentPieces > 0) {
+          if (board[parseInt(x)][parseInt(y)] == 0) {
             board[parseInt(x)][parseInt(y)] = 2;
             cell.appendChild(opponentPieces.lastChild);
             playerTurn = "player1";
             DropabbleOpponentPieces--;
           }
         }
-      } else if (phase === "Move") {
+      } else if (phase == "Move") {
         let [x, y] = cell.id.split("-");
         console.log(x, y);
-        if (playerTurn === "player1" && board[parseInt(x)][parseInt(y)] === 1) {
+        if (playerTurn == "player1" && board[parseInt(x)][parseInt(y)] == 1) {
           console.log("selected");
           if (cell.classList.contains("selected")) {
             cell.classList.remove("selected");
@@ -311,7 +313,7 @@ function startGame(difficulty) {
             selected = true;
             renderPossibleMoves(parseInt(x), parseInt(y));
           }
-        } else if (playerTurn === "player2" && board[parseInt(x)][parseInt(y)] === 2) {
+        } else if (playerTurn == "player2" && board[parseInt(x)][parseInt(y)] == 2) {
           console.log("selected");
           if (cell.classList.contains("selected")) {
             cell.classList.remove("selected");
@@ -329,7 +331,7 @@ function startGame(difficulty) {
           }
         }
       }
-      if (DropabblePlayerPieces === 0 && DropabbleOpponentPieces === 0) {
+      if (DropabblePlayerPieces == 0 && DropabbleOpponentPieces == 0) {
         phase = "Move";
       }
       messageBox.innerText = phase + " Phase " + playerTurn;
@@ -437,7 +439,7 @@ document.addEventListener("DOMContentLoaded", function () {
       generateGameBoard(parseInt(rows), parseInt(columns));
       generateGamePieces(player1Colour, player2Colour);
 
-      if (firstPlayer.textContent === "Player 1") {
+      if (firstPlayer.textContent == "Player 1") {
         playerTurn = "player1";
       } else {
         playerTurn = "player2";
