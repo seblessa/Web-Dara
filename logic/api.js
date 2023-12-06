73dba5c4c94d121e9c02329d0fab9fe09426775c
seblessa <sebastiaolessa2002@outlook.com>
2023-12-06 18:25:28 +0000

    Added pedidos

---
+++ logic/api.js
@@ -0,0 +1,78 @@
+
+
+const register = (nick, password) => {
+    fetch('http://twserver.alunos.dcc.fc.up.pt:8008//register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ nick, password }),
+    })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error('Erro:', error));
+};
+
+
+const join = (group, nick, password, size) => {
+    fetch('http://twserver.alunos.dcc.fc.up.pt:8008//join', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ group, nick, password, size }),
+    })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error('Erro:', error));
+};
+
+const leave = (nick, password, game) => {
+    fetch('http://twserver.alunos.dcc.fc.up.pt:8008//leave', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ nick, password, game }),
+    })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error('Erro:', error));
+};
+
+const notify = (nick, password, game, move) => {
+    fetch('http://twserver.alunos.dcc.fc.up.pt:8008//notify', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ nick, password, game, move }),
+    })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error('Erro:', error));
+};
+
+const update = (game, nick) => {
+    const url = `http://twserver.alunos.dcc.fc.up.pt:8008//update?game=${game}&nick=${nick}`;
+    const eventSource = new EventSource(url);
+
+    eventSource.onmessage = event => {
+        const data = JSON.parse(event.data);
+        console.log(data);
+    };
+
+    eventSource.onerror = error => {
+        console.error('Erro:', error);
+        eventSource.close();
+    };
+};
+
+
+const getRanking = (group, size) => {
+    const url = `http://twserver.alunos.dcc.fc.up.pt:8008//ranking?group=${group}&size=${size}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error('Erro:', error));
+};
